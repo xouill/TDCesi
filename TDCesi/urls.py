4d7258a53c268fcc6eed2591089977037488d2ec
Lorenzo <lorenzo.deboyer@gmail.com>
2019-02-12 16:21:48 +0100

    dossier forms reservation.py
    urls.py
    
    templates
    detail.html
    reservation.html
    Views.py

--- TDCesi/urls.py
+++ TDCesi/urls.py
@@ -16,12 +16,14 @@ Including another URLconf
 from django.contrib import admin
 from django.conf.urls import url
 from django.urls import path
-from app.views import IndexView, ConcertDetailView, ConcertListView
+from app.views import IndexView, ConcertDetailView, ConcertReservationFormView, ConcertListView
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', IndexView.as_view(), name='index'),
     url(r'detail/(?P<pk>[0-9]+)', ConcertDetailView.as_view(template_name='detail.html'), name='detail'),
+    #path('reservation', ConcertReservationFormView.as_view(), name='reservation'),
+    url(r'reservation/(?P<pk>[0-9]+)', ConcertReservationFormView.as_view(template_name='reservation.html'), name='reservation'),
     url('list', ConcertListView.as_view(template_name='list_view.html'), name='view')
 ]
--- app/views.py
+++ app/views.py
@@ -1,9 +1,11 @@
 from django.shortcuts import render
 
 # Create your views here.
-from app.models import Groupe
-from django.views.generic import TemplateView, DetailView, ListView
-from app.models import Concert
+from app.models import Groupe, Concert, PlaceVendu
+from django.views.generic import TemplateView, DetailView, ListView, FormView
+
+from app.forms.reservation import ConcertReservationForm
+from django.urls import reverse_lazy
 
 
 class IndexView(TemplateView):
@@ -18,3 +20,19 @@ class ConcertDetailView(DetailView):
 class ConcertListView(ListView):
     template_name = 'list_view.html'
     model = Groupe
+
+
+class ConcertReservationFormView(FormView):
+    form_class = ConcertReservationForm
+    template_name = 'reservation.html'
+    success_url = reverse_lazy('reservation')  # redirection quand réussi
+
+    def get_initial(self):
+        initial = super(ConcertReservationFormView, self).get_initial()
+        initial['adresseMail'] = ''
+        return initial
+
+    def form_valid(self, form):
+        # si le formulaire est valide on arrive ici
+        return super(ConcertReservationFormView, self).form_valid(form)
+        # faire la liaison avec la bdd
