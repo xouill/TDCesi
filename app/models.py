5c3100df348d04462e329dfb0727009d1f568ee6
marke <Lolmdrtg1234?>
2019-02-12 16:49:39 +0100

    Mise en place du CSS sur l'index et list concert

--- app/models.py
+++ app/models.py
@@ -5,28 +5,28 @@ from django.db import models
 
 
 class Groupe(models.Model):
-    photoDeGroupe = models.CharField(max_length=200)
-    nom = models.CharField(max_length=200)
-    genre = models.CharField(max_length=200)
-    description = models.CharField(max_length=200)
+   photoDeGroupe = models.CharField(max_length=200)
+   nom = models.CharField(max_length=200)
+   genre = models.CharField(max_length=200)
+   description = models.CharField(max_length=200)
 
 
 class Concert(models.Model):
-    intitule = models.CharField(max_length=200)
-    groupe = models.ForeignKey(Groupe,
-                               on_delete=models.CASCADE,
-                               default="none",
-                               related_name='groupe')
-    lieux = models.CharField(max_length=200)
-    prix = models.IntegerField()
-    placeMax = models.IntegerField()
+   intitule = models.CharField(max_length=200)
+   groupe = models.ForeignKey(Groupe,
+                              on_delete=models.CASCADE,
+                              default="none",
+                              related_name='groupe')
+   lieux = models.CharField(max_length=200)
+   prix = models.IntegerField()
+   placeMax = models.IntegerField()
 
 
 class PlaceVendu(models.Model):
-    concert = models.ForeignKey(Concert,
-                                on_delete=models.DO_NOTHING,
-                                default="none",
-                                related_name='concert'
-                                )
-    adresseMail = models.CharField(max_length=200)
-    place = models.IntegerField()
+   concert = models.ForeignKey(Concert,
+                               on_delete=models.DO_NOTHING,
+                               default="none",
+                               related_name='concert'
+                               )
+   adresseMail = models.CharField(max_length=200)
+   place = models.IntegerField()
